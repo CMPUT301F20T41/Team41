ae9cc15c04ed8307c32a674972a5f810b66bf265
mingyaang <mmao@ualberta.ca>
2020-10-02 11:07:26 -0600

    >:(

--- app/src/main/java/com/example/team41/Shape.java
+++ app/src/main/java/com/example/team41/Shape.java
@@ -1,11 +1,7 @@
 package com.example.team41;
 
 public abstract class Shape {
-<<<<<<< HEAD
 	String color = "transparent";
-=======
-    String color = "blue";
->>>>>>> master
     private int x;
     private int y;
 }
